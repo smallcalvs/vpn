95c96e8e423b4ba35b64bdd0d0acd3dd3b34310c
smallcalvs <81384454+smallcalvs@users.noreply.github.com>
2021-03-25 14:35:59 -0500

    Create sql.php

---
+++ logging/sql.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "localhost";
+$username = "[username]";
+$password = "[password]";
+$dbname = "[database]";
+$time = date("Y-m-d H:i:s") . " UTC";
+$ip = $_SERVER["HTTP_CF_CONNECTING_IP"];
+$country = $_SERVER["HTTP_CF_IPCOUNTRY"];
+?>
